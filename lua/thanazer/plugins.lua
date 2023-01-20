e04b8dd93945e6e158547712095fc99b23e15394
Aneesh Dandime <aneesh.dandime9@gmail.com>
2023-01-20 15:20:16 -0500

    Completed basic setup.

---
+++ lua/thanazer/plugins.lua
@@ -0,0 +1,93 @@
+-- Bootstrapping packer
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+-- Autocommand to sync plugins on saving this file
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]])
+
+-- Plugins to be used
+return require('packer').startup({function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- Colorscheme
+  use 'lunarvim/darkplus.nvim'
+
+  -- Telescope fuzzy file finding
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  -- Syntax highlighting using treesitter and playground
+  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use('nvim-treesitter/playground')
+
+  -- Automatically pair parens, quotes, etc.
+  use {
+    'windwp/nvim-autopairs',
+      config = function() require('nvim-autopairs').setup {} end
+  }
+
+  -- Undotree
+  use('mbbill/undotree')
+
+  -- LazyGit from within neovim
+  use 'kdheepak/lazygit.nvim'
+
+  -- LSP
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v1.x',
+    requires = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},             -- Required
+      {'williamboman/mason.nvim'},           -- Optional
+      {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},         -- Required
+      {'hrsh7th/cmp-nvim-lsp'},     -- Required
+      {'hrsh7th/cmp-buffer'},       -- Optional
+      {'hrsh7th/cmp-path'},         -- Optional
+      {'saadparwaiz1/cmp_luasnip'}, -- Optional
+      {'hrsh7th/cmp-nvim-lua'},     -- Optional
+
+      -- Snippets
+      {'L3MON4D3/LuaSnip'},             -- Required
+      {'rafamadriz/friendly-snippets'}, -- Optional
+    }
+  }
+
+  -- Zen mode
+  use 'folke/zen-mode.nvim'
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end,
+-- Use floating window for command outputs
+config = {
+  display = {
+    open_fn = function()
+      return require('packer.util').float({ border = 'single' })
+    end
+  }
+}})
